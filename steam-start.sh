0e2f0237595540214a4346ac6c1cfd527531cc01
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2021-03-06 14:43:32 +0200

    Fixes

--- steam-start.sh
+++ steam-start.sh
@@ -52,14 +52,14 @@ fi
 [ ! -z "${STEAM_RUNTIME}" ] && echo "STEAM_RUNTIME=${STEAM_RUNTIME}"
 
 if [ "$STEAM_RUNTIME" != "0" ]; then
-    set_var SSL_CERT_DIR="/etc/ssl/certs"
-    #set_var LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so'
-    #set_var LD_PRELOAD='./libcxxrt.so:/usr/$LIB/libstdc++.so.6'
+    set_var SSL_CERT_DIR "/etc/ssl/certs"
+    #set_var LD_PRELOAD '/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so'
+    #set_var LD_PRELOAD './libcxxrt.so:/usr/$LIB/libstdc++.so.6'
 fi
 
 if [ "${CPU_VENDOR}" == "Intel" ] && [ ${CPU_MODEL_NUMBER} -le 58 ]; then
     echo "DXVK disabled for this CPU (${CPU_VENDOR} model ${CPU_MODEL_NUMBER})"
-    set_var PROTON_USE_WINED3D=1
+    set_var PROTON_USE_WINED3D 1
 fi
 
 if ${IS_LAPTOP} && ${HAS_OPTIMUS_SUPPORT} && [ ${BATT_STATE} != "Discharging" ]; then # && [ $STEAM_RUNTIME = 0 ]; then
