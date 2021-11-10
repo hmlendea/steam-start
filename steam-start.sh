c4be9730fdd3580466dac9af2e77e156d52ff7be
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-11-11 00:37:26 +0200

    Improved hardware information gathering

--- steam-start.sh
+++ steam-start.sh
@@ -1,55 +1,97 @@
 #!/bin/bash
 
-IS_LAPTOP=false
-HAS_OPTIMUS_SUPPORT=false
-BATT_STATE="Unknown"
 STEAM_FRAME_FORCE_CLOSE=1
-ALLOW_VSYNC_OFF=0
-GPU_VENDOR="intel"
-CPU_VENDOR=$(cat /proc/cpuinfo | grep "^vendor_id" | head -n 1 | awk -F: '{print $2}' | sed 's/ //g' | sed 's/^Genuine//g')
-CPU_MODEL_NUMBER=$(cat /proc/cpuinfo | grep "^model" | head -n 1 | awk -F: '{print $2}' | sed 's/ //g')
-STEAM_EXECUTABLE="/usr/lib/steam/steam"
+ALLOW_VSYNC_OFF=false
 
-# Fix for loosing focus in BPM after exiting a game
-export SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS=0
+#####################################
+### Retrieve hardware information ###
+#####################################
 
-export VERSION_ID="1"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/usr/lib}:/usr/lib32"
+CHASSIS_TYPE="Desktop"
+BATT_STATE="Unknown"
+HAS_OPTIMUS_SUPPORT=false
 
-[ $(lspci | grep "VGA" | grep "NVIDIA" -c) -ge 1 ] && GPU_VENDOR="nvidia"
+if [ -d "/sys/module/battery" ] \
+&& [ -d "/proc/acpi/button/lid" ]; then
+    CHASSIS_TYPE="Laptop"
+    BATTERY_STATE=$(cat /sys/class/power_supply/BAT0/status)
+fi
 
-echo "GPU Vendor: ${GPU_VENDOR}"
+if [ -f "/usr/bin/lscpu" ]; then
+    CPU_MODEL=$(lscpu | \
+        grep "^Model name:" | \
+        awk -F: '{print $2}')
+elif [ -f "/proc/cpuinfo" ]; then
+    CPU_MODEL=$(cat "/proc/cpuinfo" | \
+        grep "^model name" | \
+        awk -F: '{print $2}')
+fi
+
+CPU_MODEL=$(echo "${CPU_MODEL}" | head -n 1 | sed \
+    -e 's/^\s*\(.*\)\s*$/\1/g' \
+    -e 's/(TM)//g' \
+    -e 's/(R)//g' \
+    -e 's/ [48][ -][Cc]ore//g' \
+    -e 's/ \(CPU\|Processor\)//g' \
+    -e 's/@ .*//g' \
+    -e 's/^[ \t]*//g' \
+    -e 's/[ \t]*$//g')
+
+echo "${CPU_MODEL}" | grep -q "AMD" && CPU_FAMILY="AMD"
+echo "${CPU_MODEL}" | grep -q "Intel" && CPU_FAMILY="Intel"
+
+CPU_MODEL=$(echo "${CPU_MODEL}" | sed 's/\(AMD\|Intel\) //g')
+CPU_NAME=$(echo "${CPU_FAMILY} ${CPU_MODEL}" | sed 's/^\s*//g')
+
+if [ -f "/usr/bin/lspci" ]; then
+    LSPCI_VGA_PRIMARY="$(lspci | grep VGA | tail -n 1)"
+    echo "${LSPCI_VGA_PRIMARY}" | grep -q "AMD"      && GPU_FAMILY="AMD"
+    echo "${LSPCI_VGA_PRIMARY}" | grep -q "Intel"    && GPU_FAMILY="Intel"
+    echo "${LSPCI_VGA_PRIMARY}" | grep -q "NVIDIA"   && GPU_FAMILY="Nvidia"
+    
+    GPU_MODEL=$(echo "${LSPCI_VGA_PRIMARY}" | sed 's/^[^\[]*\[\([a-zA-Z0-9 ]*\)].*/\1/g')
+    GPU_NAME=$(echo "${GPU_FAMILY} ${GPU_MODEL}" | sed 's/^\s*//g')
+fi
+
+if [[ "${GPU_FAMILY}" == "Nvidia" ]]; then
+    if [ -f "/usr/bin/bumblebeed" ]; then
+        HAS_OPTIMUS_SUPPORT=true
+    fi
+fi
+
+### Print hardware information
+echo "Detected hardware:"
+echo " - Chassis: ${CHASSIS_TYPE}"
+[[ "${CHASSIS_TYPE}" == "Laptop" ]] && echo " - Battery: ${BATTERY_STATE}"
+echo " - CPU: ${CPU_NAME}"
+echo " - GPU: ${GPU_NAME}"
+[[ "${GPU_FAMILY}" == "Nvidia" ]] && echo " - Supports OPTIMUS: ${HAS_OPTIMUS_SUPPORT}"
+[ -n "${STEAM_RUNTIME}" ] && echo "Steam Runtime: ${STEAM_RUNTIME}"
+
+### Functions
 
 function set_var() {
-    VARIABLE_NAME="${1}"
-    VARIABLE_VALUE="${@:2}"
+    local VARIABLE_NAME="${1}"
+    local VARIABLE_VALUE="${@:2}"
 
     export ${VARIABLE_NAME}="${VARIABLE_VALUE}"
 
     echo "Variable '${VARIABLE_NAME}' set to '${VARIABLE_VALUE}'"
 }
 
-if [ -d "/sys/class/power_supply/BAT0" ]; then
-    IS_LAPTOP=true
-    BATT_STATE=$(cat /sys/class/power_supply/BAT0/status)
+### Set environment variables
 
-    echo "Battery state is '$BATT_STATE'"
-
-    if [ "$GPU_VENDOR" == "nvidia" ]; then
-        if [ -f "/usr/bin/bumblebeed" ]; then
-            echo "OPTIMUS support detected..."
-            HAS_OPTIMUS_SUPPORT=true
-        fi
-
-        if [ "$BATT_STATE" == "Unknown" ] && [ $ALLOW_VSYNC_OFF == 1 ]; then
-            set_var vblank_mode 0
-        else
-            set_var vblank_mode 1
-        fi
+if [[ "${CHASSIS_TYPE}" == "Laptop" ]]; then
+    if [ "$BATTERY_STATE" == "Unknown" ] && ${ALLOW_VSYNC_OFF}; then
+        set_var vblank_mode 0
+    else
+        set_var vblank_mode 1
     fi
 fi
 
-[ ! -z "${STEAM_RUNTIME}" ] && echo "STEAM_RUNTIME=${STEAM_RUNTIME}"
+set_var SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS 0 # Fix for loosing focus in BPM after exiting a game
+set_var VERSION_ID 1
+set_var LD_LIBRARY_PATH "${LD_LIBRARY_PATH:-/usr/lib}:/usr/lib32"
 
 if [ "$STEAM_RUNTIME" != "0" ]; then
     set_var SSL_CERT_DIR "/etc/ssl/certs"
@@ -60,12 +102,12 @@ if [ "$STEAM_RUNTIME" != "0" ]; then
     #set_var LD_PRELOAD './libcxxrt.so:/usr/$LIB/libstdc++.so.6'
 fi
 
-if [ "${CPU_VENDOR}" == "Intel" ] && [ ${CPU_MODEL_NUMBER} -le 58 ]; then
-    echo "DXVK disabled for this CPU (${CPU_VENDOR} model ${CPU_MODEL_NUMBER})"
+if [ "${CPU_FAMILY}" == "Intel" ] && [ "${CPU_MODEL}" == "Core i7-3610QM" ]; then
+    echo "DXVK does not work on this CPU"
     set_var PROTON_USE_WINED3D 1
 fi
-
-if ${IS_LAPTOP} && ${HAS_OPTIMUS_SUPPORT} && [ ${BATT_STATE} != "Discharging" ]; then # && [ $STEAM_RUNTIME = 0 ]; then
+exit
+if ${HAS_OPTIMUS_SUPPORT} && [ ${BATTERY_STATE} != "Discharging" ]; then # && [ $STEAM_RUNTIME = 0 ]; then
     optiprime $STEAM_EXECUTABLE
 else
     $STEAM_EXECUTABLE $* -fulldesktopres
